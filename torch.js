5a086b9127a9f79853ca28f3bf91ae3d5a4b0238
cocktailpeanut <cocktailpeanuts@proton.me>
2025-04-09 20:22:13 -0700

    update

--- torch.js
+++ torch.js
@@ -21,12 +21,12 @@ module.exports = {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
-          "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu126",
-          "uv pip install https://huggingface.co/cocktailpeanut/fa/resolve/main/flash_attn-2.7.0.post2-cp310-cp310-win_amd64.whl"
-//          "uv pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu126",
+          //"uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu126",
+          //"uv pip install https://huggingface.co/cocktailpeanut/fa/resolve/main/flash_attn-2.7.0.post2-cp310-cp310-win_amd64.whl"
+          "uv pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu126",
           //"uv pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu124",
           //"uv pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu121",
-          //"uv pip install https://huggingface.co/lldacing/flash-attention-windows-wheel/resolve/main/flash_attn-2.7.4%2Bcu126torch2.6.0cxx11abiFALSE-cp310-cp310-win_amd64.whl"
+          "uv pip install https://huggingface.co/lldacing/flash-attention-windows-wheel/resolve/main/flash_attn-2.7.4%2Bcu126torch2.6.0cxx11abiFALSE-cp310-cp310-win_amd64.whl"
           //"uv pip install https://github.com/sunsetcoder/flash-attention-windows/raw/refs/heads/main/flash_attn-2.7.0.post2-cp310-cp310-win_amd64.whl --no-build-isolation"
           //"uv pip install https://github.com/zac-hopkinson/fa2-cu121-ll/releases/download/v2.5.9.post1-torch2.5-cu121-cp310/flash_attn-2.5.9.post1-cp310-cp310-linux_x86_64.whl"
         ]
